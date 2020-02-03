5685c1f8d1a5acdfa96c416d136c4408a8049556
William Eastbury <William.eastbury@microsoft.com>
2020-02-03 22:08:53 +0000

    Update buildstatic.sh

--- webroot/blogpages/buildstatic.sh
+++ webroot/blogpages/buildstatic.sh
@@ -1,2 +1,4 @@
-curl https://api.github.com/markdown/raw -X "POST" -H "Content-Type: text/x-markdown" --data-binary "@C:/DataStore/Source Code/WILLEASTBURY.COM/willeastbury.com/webroot/blogpages/home.md" >> home.html
-curl https://api.github.com/markdown/raw -X "POST" -H "Content-Type: text/x-markdown" --data-binary "@C:/DataStore/Source Code/WILLEASTBURY.COM/willeastbury.com/webroot/blogpages/cloud.md" >> cloud.html
+curl https://api.github.com/markdown/raw -X "POST" -H "Content-Type: text/x-markdown" --data-binary "@C:/DataStore/Source Code/WILLEASTBURY.COM/willeastbury.com/webroot/blogpages/home.md" >> home.fragment
+curl https://api.github.com/markdown/raw -X "POST" -H "Content-Type: text/x-markdown" --data-binary "@C:/DataStore/Source Code/WILLEASTBURY.COM/willeastbury.com/webroot/blogpages/cloud.md" >> cloud.fragment
+cat template.start cloud.fragment template.finsh > ./cloud.html
+cat template.start home.fragment template.finsh > ./home.html
