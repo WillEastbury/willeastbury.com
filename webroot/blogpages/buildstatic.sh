38e48275856660947803db8135607585c0676b4e
William Eastbury <William.eastbury@microsoft.com>
2020-02-03 21:54:53 +0000

    adding buildstatic

---
+++ webroot/blogpages/buildstatic.sh
@@ -0,0 +1,2 @@
+curl https://api.github.com/markdown/raw -X "POST" -H "Content-Type: text/x-markdown" --data-binary "@C:/DataStore/Source Code/WILLEASTBURY.COM/willeastbury.com/webroot/blogpages/home.md"
+curl https://api.github.com/markdown/raw -X "POST" -H "Content-Type: text/x-markdown" --data-binary "@C:/DataStore/Source Code/WILLEASTBURY.COM/willeastbury.com/webroot/blogpages/cloud.md"
