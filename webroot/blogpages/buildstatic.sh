84699ac8f759849e6e4434234388b4d11e6c1c39
William Eastbury <William.eastbury@microsoft.com>
2020-02-03 22:30:09 +0000

    Update buildstatic.sh

--- webroot/blogpages/buildstatic.sh
+++ webroot/blogpages/buildstatic.sh
@@ -1,3 +1,5 @@
+cd /home/runner/work/willeastbury.com/willeastbury.com/webroot/blogpages
+
 curl https://api.github.com/markdown/raw -X "POST" -H "Content-Type: text/x-markdown" --data-binary "@home.md" >> home.fragment
 cat template.start home.fragment template.finish > home.html
 
